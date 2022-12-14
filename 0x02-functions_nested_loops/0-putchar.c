edef7af932b5733889274a89c7549c63ef0eb7ba
ralphvwilliams <85120870+ralphvwilliams@users.noreply.github.com>
2022-12-14 12:04:22 +0000

    Add files via upload

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void) {
+	char s[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int length = 8;
+	for(int i = 0; i < length; i++) {
+		_putchar(s[i])
+	}
+	return 0;
+}
